212819461f0244ff5bb925871e9447a9e0067545
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-02 06:37:25 -0800

    Write pseudocode describing input 'user action' output 'CSS change'

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,15 @@
+/* Pseudocode 
+
+    identify and select all keys 
+    turn this selection into array (NodeList)
+
+    create function that iterates through array 
+    function returns el associated with data-key
+
+    create another function
+    this function changes CSS of Element 
+    
+    set event checkpoint to listen for user action
+    once observed, execute change CSS function
+
+*/
